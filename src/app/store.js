6db056015c9f6fcef6b9325fc365584ab1f22191
= <=>
2022-10-06 01:24:21 +0700

    Ready to deploy

--- src/app/store.js
+++ src/app/store.js
@@ -4,13 +4,13 @@ import { setupListeners } from "@reduxjs/toolkit/query";
 import authReducer from "../features/auth/authSlice";
 
 export const store = configureStore({
-  reducer: {
-    [apiSlice.reducerPath]: apiSlice.reducer,
-    auth: authReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware),
-  devTools: true,
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer,
+        auth: authReducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: false,
 });
 
 setupListeners(store.dispatch);
